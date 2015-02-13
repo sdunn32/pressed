5460b4da980e261a53de312a96f3603c2323d5cb
sdunn32 <sdunn@mcad.edu>
2015-02-12 21:54:22 -0600

    Wk 4 assignment
    adding header, footer and sidebar calls

--- index.php
+++ index.php
@@ -1,42 +1,18 @@
-<!DOCTYPE html>
-<html>
-<html>
-    <head>
-        <title><?php bloginfo('name'); ?></title>
-				<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/images/hero.jpg" >
-	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
-	<script src="http://use.edgefonts.net/merriweather.js"></script>
-</head>
-	<body>
-		<header>
-			<h1><?php bloginfo('name'); ?></h1>
-			<nav>
-				<ul>
-					<li>Home</li>
-					<li>WordPress</li>
-				</ul>
-			</nav>
-		</header>
-		<section>
-			<img src="http://localhost:8888/wp-content/themes/pressed/images/hero.jpg" />
+<?php get_header(); ?>
+<div class="clearfix">
+		<section class="main">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/hero.jpg" />
 			<?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>
 			<section>
 			<a href"<?php the_permalink(); ?>"><h1><?php the_title_attribute(); ?></h1>
 			<?php the_content(); ?>	
 			<!--<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
-				<p>We also make pancakes on the side.</p>
-			-->
+				<p>We also make pancakes on the side.</p> -->
 			</section>
 			<?php endwhile; else: ?>
 			<p><?php _e('sorry no content'); ?></p>
 			<?php endif; ?>
-			
-		</section>
-		<footer>
-			<h1>Contact Us</h1>
-			<p>Pressed and Squeezed</p>
-			<p>2525 Orange Way, Orchard, CA 91104</p>
-			<p>Phone: (800) 424-3232</p>
-		</footer>
-	</body>
-</html>
+	</section>
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
